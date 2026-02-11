b5e2c5ed7415fa21c50947cad779e2bc0f2024e7
Rodrigo Sousa <rodrigodss89@gmail.com>
2026-02-11 13:20:20 -0300

    Initial Cypress project setup

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'srx2ud',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    baseUrl: "https://loginxp.vercel.app"
+  }
+});
